e45eb76dca3020ec6dc6108fcd584c79abe68016
re770061 <re770061@aragon.iem>
2021-01-16 17:08:20 +0100

    First commit

--- src/main/java/GRACE/Disque.java
+++ src/main/java/GRACE/Disque.java
@@ -15,4 +15,21 @@ public class Disque {
     private ArrayList<Table> tables;
     private ArrayList<Table> tablesR;
     private ArrayList<Table> tablesS;
+    
+    public Disque(ArrayList<Table> tables, ArrayList<Table> tablesR, ArrayList<Table> tablesS)
+    {
+        this.tables = tables;
+        this.tablesR = tablesR;
+        this.tablesS = tablesS;
+    }
+    
+    public Table getTableR()
+    {
+        return this.tables.get(0);
+    }
+    
+    public Table getTableS()
+    {
+        return this.tables.get(1);
+    }
 }
